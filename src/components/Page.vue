f3684042e5a9938802877b27110f6eed3af0d5e7
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-12-21 13:10:18 +0100

    Added defautl page component

---
+++ src/components/Page.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="page">
+		<main :class="padded ? 'padded' : ''">
+			<slot></slot>
+		</main>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "",
+		props: ["padded"],
+		components: {},
+	};
+</script>
+
+<style scoped>
+	.page {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		flex-grow: 1;
+		flex-direction: column;
+	}
+	.page main {
+		display: flex;
+		flex-grow: 1;
+	}
+	.page main.padded {
+		padding: 30px;
+	}
+</style>
