d404d37e16d13e815648be087dbcfeeaa35fe5db
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-12-22 10:57:35 +0100

    added Shows page

---
+++ src/views/Shows.vue
@@ -0,0 +1,20 @@
+<template>
+	<Page>
+		coucou
+	</Page>
+</template>
+
+<script>
+	import Page from "@/components/Page.vue";
+	export default {
+		name: "Shows",
+		props: [],
+		components: {Page},
+		methods: {},
+		data() {
+			return {};
+		},
+	};
+</script>
+
+<style scoped></style>
