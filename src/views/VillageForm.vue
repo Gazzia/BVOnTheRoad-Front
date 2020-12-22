3043a33f33d714aa30e66c7c55e0079cd132afe1
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-12-22 09:32:35 +0100

    added VillageFrom component

---
+++ src/views/VillageForm.vue
@@ -0,0 +1,20 @@
+<template>
+	<Page :padded="true">
+		Test
+	</Page>
+</template>
+
+<script>
+	import Page from "@/components/Page.vue";
+	export default {
+		name: "VillageForm",
+		props: [],
+		components: {Page},
+		methods: {},
+		data() {
+			return {};
+		},
+	};
+</script>
+
+<style scoped></style>
