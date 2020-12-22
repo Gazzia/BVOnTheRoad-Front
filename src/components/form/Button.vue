f3ae40d5c538f83a293efcfa95be9c3f542dc597
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-12-22 10:47:23 +0100

    add form components

---
+++ src/components/form/Button.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="button" :class="{disabled}"><slot></slot></div>
+</template>
+
+<script>
+	export default {
+		name: "Button",
+		props: ["disabled"],
+		components: {},
+		methods: {},
+	};
+</script>
+
+<style scoped>
+	.button {
+		margin-top: 6px;
+		padding: 10px 15px;
+		box-sizing: border-box;
+		border-radius: 6px;
+		color: #42f056;
+		border: 1px solid currentColor;
+		user-select: none;
+		cursor: pointer;
+		transition: 0.2s;
+
+		font-weight: 600;
+		font-size: 12px;
+		letter-spacing: 0.4px;
+		display: flex;
+		justify-content: center;
+	}
+	.button:active {
+		transform: scale(0.95);
+	}
+	.button.disabled {
+		color: grey;
+	}
+	.button.disabled:active {
+		transform: scale(1);
+	}
+</style>
---
+++ src/components/form/TextInput.vue
@@ -0,0 +1,31 @@
+<template>
+	<input
+		:value="modelValue"
+		@input="$emit('update:modelValue', $event.target.value)"
+		:placeholder="placeholder"
+		:class="{required}"
+	/>
+</template>
+
+<script>
+	export default {
+		name: "TextInput",
+		props: ["modelValue", "placeholder", "required"],
+		emits: ["update:modelValue"],
+	};
+</script>
+
+<style scoped>
+	input {
+		border: none;
+		padding: 10px 20px;
+		font-size: 14px;
+		border-radius: 6px;
+		font-family: "Poppins", sans-serif;
+		color: #181d23;
+		box-sizing: border-box;
+	}
+	input.required {
+		border: 3px solid #42f056;
+	}
+</style>
