f025df8e135724e205929f2e9665dcbb69583216
Joseph Allain <36985411+Gazzia29@users.noreply.github.com>
2020-12-22 10:47:35 +0100

    filled village form

--- src/views/VillageForm.vue
+++ src/views/VillageForm.vue
@@ -1,20 +1,96 @@
 <template>
-	<Page :padded="true">
-		Test
+	<Page :padded="true" :centered="true">
+		<div class="title">Ajout d'un village</div>
+		<div class="form">
+			<TextInput
+				v-model="village.name"
+				placeholder="Nom du village"
+				:required="true"
+			></TextInput>
+			<TextInput
+				v-model="village.postCode"
+				placeholder="Code postal"
+				:required="true"
+			></TextInput>
+			<TextInput
+				v-model="village.imageUrl"
+				placeholder="URL d'une photo (optionnel)"
+			></TextInput>
+			<Button :disabled="!allFieldsAreFilled()" @click="validateAndPostVillage()">{{
+				allFieldsAreFilled() ? "Ajouter" : "Remplissez les champs obligatoires"
+			}}</Button>
+		</div>
 	</Page>
 </template>
 
 <script>
 	import Page from "@/components/Page.vue";
+	import TextInput from "@/components/form/TextInput.vue";
+	import Button from "@/components/form/Button.vue";
+
 	export default {
 		name: "VillageForm",
-		props: [],
-		components: {Page},
-		methods: {},
+		props: ["villages"],
+		components: {Page, TextInput, Button},
+		emits: ["fetchVillages"],
+		methods: {
+			allFieldsAreFilled() {
+				let nameFilled = this.village.name.trim() != "";
+				let postCodeFilled = this.village.postCode.trim() != "";
+				return nameFilled && postCodeFilled;
+			},
+			validateAndPostVillage() {
+				const travel = () => {
+					this.$emit("fetchVillages");
+					this.$router.push("/villages");
+				};
+				if (this.allFieldsAreFilled()) {
+					fetch("https://breizhvideo.herokuapp.com/villages", {
+						method: "POST",
+						mode: "cors",
+						keepalive: true,
+						headers: {
+							"Content-type": "application/json; charset=UTF-8",
+						},
+						body: JSON.stringify(this.village),
+					})
+						.then((response) => {
+							if (response.ok) {
+								return response.json();
+							}
+							return Promise.reject(response);
+						})
+						.then(function(data) {
+							console.log(data);
+							travel();
+						})
+						.catch(function(error) {
+							console.warn("Something went wrong.", error);
+						});
+				}
+			},
+		},
 		data() {
-			return {};
+			return {
+				village: {
+					name: "",
+					postCode: "",
+					imageUrl: "",
+				},
+			};
 		},
 	};
 </script>
 
-<style scoped></style>
+<style scoped>
+	.title {
+		font-family: "Satisfy", sans-serif;
+		font-size: 45px;
+	}
+	.form {
+		margin-top: 40px;
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+	}
+</style>
